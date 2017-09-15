5f56232feaf5ca385f6477e0a40adc0124ff397e
livorish <hoksini@gmail.com>
2017-09-15 11:13:56 +0300

    styles complete

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
-var gulp = require('gulp'),
+const gulp = require('gulp'),
     sass = require('gulp-sass');
 gulp.task('styles', () => {
     return gulp.src('sass/styles.scss')
+    .on('error', sass.logError)
     .pipe(sass())
     .pipe(gulp.dest('./css'));
 }); 
